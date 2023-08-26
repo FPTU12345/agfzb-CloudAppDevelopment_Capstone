640c0530b773fd57cda11a90906ee16b87615dc7
FPTU12345 <114289940+FPTU12345@users.noreply.github.com>
2023-08-26 11:39:19 +0700

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path(route='login/', view=views.login_request, name='login'),
     path(route='logout/', view=views.logout_request, name='logout'),
     path(route='dealer/<int:dealer_id>/', view=views.get_dealer_details, name='dealer_details'),
-    path(route='dealer/<int:dealer_id>/add-review/', view=views.add_review, name="add_review")
+    path(route='dealer/<int:dealer_id>/add-review/', view=views.add_review, name='add_review')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
