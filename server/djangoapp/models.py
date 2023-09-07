5f5436921a3a4108d3786f6266a091369084f2e3
FPTU12345 <114289940+FPTU12345@users.noreply.github.com>
2023-09-07 15:38:51 +0700

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,82 +1,55 @@
-import datetime
 from django.db import models
 from django.utils.timezone import now
 
 class CarMake(models.Model):
-    name = models.CharField(null=False, max_length=50)
-    description = models.CharField(null=True, max_length=500)
+    name = models.CharField(null= False, max_length=30, default='Porsche')
+    description = models.CharField(null= False, max_length=300, default='Porsche owns VW and VW owns Porsche')
 
     def __str__(self):
-        return self.name
+        return 'Name:' + self.name + ',' + \
+            'Description:' + self.description
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
-    name = models.CharField(null=False, max_length=50)
-    dealer_id = models.IntegerField(null=True)
-
-    SEDAN = "Sedan"
-    SUV = "SUV"
-    WAGON = "Wagon"
-    SPORT = "Sport"
-    COUPE = "Coupe"
-    MINIVAN = "Mini"
-    VAN = "Van"
-    PICKUP = "Pickup"
-    TRUCK = "Truck"
-    BIKE = "Bike"
-    SCOOTER = "Scooter"
-    OTHER = "Other"
-    CAR_CHOICES = [(SEDAN, "Sedan"), 
-                   (SUV, "SUV"), 
-                   (WAGON, "Station wagon"), 
-                   (SPORT, "Sports Car"),
-                   (COUPE, "Coupe"), 
-                   (MINIVAN, "Mini van"), 
-                   (VAN, "Van"), 
-                   (PICKUP, "Pick-up truck"),
-                   (TRUCK, "Truck"), 
-                   (BIKE, "Motor bike"), 
-                   (SCOOTER, "Scooter"), 
-                   (OTHER, 'Other')]
-    model_type = models.CharField(null=False, max_length=15, choices=CAR_CHOICES, default=SEDAN)
-
-    YEAR_CHOICES = []
-    for r in range(1969, (datetime.datetime.now().year+1)):
-        YEAR_CHOICES.append((r, r))
-    year = models.IntegerField(('year'), choices=YEAR_CHOICES, default=datetime.datetime.now().year)
+    SEDAN = 'sedan'
+    SUV = 'suv'
+    WAGON = 'wagon'
+    OTHERS = 'others'
+    CAR_CHOICES = [(SEDAN, "Sedan"), (SUV, 'SUV'), (WAGON, 'Wagon'), (OTHERS, 'Others')]
+    carmake = models.ForeignKey(CarMake, null= True, on_delete=models.CASCADE)
+    name = models.CharField(null= False, max_length=30, default='Taycan')
+    dealerid = models.IntegerField(null=True)
+    cartype = models.CharField(null= False, max_length=20, choices= CAR_CHOICES, default=SEDAN)
+    year = models.DateField(null= True)
 
     def __str__(self):
-        return self.name + ", " + str(self.year) + ", " + self.model_type
+        return 'Name ' + self.name
 
 class CarDealer:
-    def __init__(self, address, city, full_name, id, lat, long, short_name, st, state, zip):
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
         self.address = address
         self.city = city
-        self.full_name = full_name  
-        self.id = id  
+        self.full_name = full_name
+        self.id = id
         self.lat = lat
         self.long = long
         self.short_name = short_name
-        self.st = st  
-        self.state = state  
+        self.st = st
         self.zip = zip
-        self.idx = 0
 
     def __str__(self):
-        return self.full_name + ", " + self.state
-
+        return "Dealer name: " + self.full_name
+        
 class DealerReview:
-    def __init__(self, dealership, id, name, purchase, review, car_make=None, car_model=None, car_year=None, purchase_date=None, sentiment="neutral"):
+    def __init__(self, name, dealership, review, purchase, purchase_date, car_make, car_model, car_year, sentiment):
+        self.name = name
+        self.dealership = dealership
+        self.review = review
+        self.purchase = purchase
+        self.purchase_date = purchase_date
         self.car_make = car_make
         self.car_model = car_model
         self.car_year = car_year
-        self.dealership = dealership
-        self.id = id  
-        self.name = name  
-        self.purchase = purchase  
-        self.purchase_date = purchase_date
-        self.review = review  
-        self.sentiment = sentiment  
+        self.sentiment = sentiment
 
     def __str__(self):
-        return "Reviewer: " + self.name + " Review: " + self.review
+        return "Review: " + self.review
