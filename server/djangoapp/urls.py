257028e06df54c16950cb219f4ac23c8c6bd4c44
FPTU12345 <114289940+FPTU12345@users.noreply.github.com>
2023-09-07 15:39:59 +0700

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,12 +5,12 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
+    path(route='about', view=views.about, name='about'),
+    path(route='contact', view=views.contact, name='contact'),
+    path(route='signup', view=views.registration_request, name='registration'),
+    path(route='login', view=views.login_request, name='login'),
+    path(route='logout', view=views.logout_request, name='logout'),
     path(route='', view=views.get_dealerships, name='index'),
-    path(route='about/', view=views.about, name='about'),
-    path(route='contact/', view=views.contact, name='contact'),
-    path(route='registration/', view=views.registration_request, name='registration'),
-    path(route='login/', view=views.login_request, name='login'),
-    path(route='logout/', view=views.logout_request, name='logout'),
-    path(route='dealer/<int:dealer_id>/', view=views.get_dealer_details, name='dealer_details'),
-    path(route='dealer/<int:dealer_id>/add-review/', view=views.add_review, name='add_review')
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path(route='dealer/<int:dealer_id>/review', view=views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
